45c1340d31494fcbc66e2bd2cf5ae0b5301170ae
Harry-Thasarathan <38916729+Harry-Thasarathan@users.noreply.github.com>
2019-02-05 18:46:11 -0500

    Add files via upload

---
+++ pix2pix-temporal/data.py
@@ -0,0 +1,26 @@
+
+from os.path import join
+from dataset import DatasetFromFolder
+from torch.utils.data import DataLoader
+
+def get_training_set(root_dir):
+    train_dir = join(root_dir, "Train")
+
+    return DatasetFromFolder(train_dir)
+
+
+def get_test_set(root_dir):
+    test_dir = join(root_dir, "Test")
+
+    return DatasetFromFolder(test_dir)
+
+def create_iterator(sample_size, sample_dataset):
+    while True:
+        sample_loader = DataLoader(
+            dataset= sample_dataset,
+            batch_size=sample_size,
+            drop_last=True
+        )
+
+        for item in sample_loader:
+            yield item
