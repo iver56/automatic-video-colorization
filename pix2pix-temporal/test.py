9cbae8503b779e7dafdf76de36b2bd9a2f76565a
Harrish <harrish.thasarathan@uoit.net>
2019-02-06 13:15:45 -0500

    Eval method works in test.py!

--- pix2pix-temporal/test.py
+++ pix2pix-temporal/test.py
@@ -1,7 +1,10 @@
 from __future__ import print_function
 from PIL import Image
+from skimage import io, feature, color, util
+from othernetworks import InpaintGenerator
 import argparse
 import os
+import numpy as np
 
 import torch
 from torch.autograd import Variable
@@ -17,10 +20,12 @@ parser.add_argument('--cuda', action='store_true', help='use cuda')
 opt = parser.parse_args()
 print(opt)
 
-netG = torch.load(opt.model)
+checkpoint = torch.load(opt.model)
+netG = InpaintGenerator()
+netG.load_state_dict(checkpoint['generator'])
 
 #image_dir = "dataset/{}/test/a/".format(opt.dataset)
-image_dir = "/work/research/git/Temporal-Anime/dataset/Temporal/Val/"
+image_dir = "/home/paperspace/Desktop/Colorization/DBZ_Dataset/{}/Val/".format(opt.dataset)
 image_filenames = [x for x in os.listdir(image_dir) if is_image_file(x)]
 
 transform_list = [transforms.ToTensor(),
@@ -30,11 +35,16 @@ transform = transforms.Compose(transform_list)
 
 #first previous frame needs to be zero image
 initial = Image.new("RGB",[256,256])
+initial = transform(initial)
 previous = Variable(initial,volatile = True).view(1,-1,256,256)
 previous.cuda()
 
 for image_name in image_filenames:
     img = load_img(image_dir + image_name)
+    img = color.rgb2gray(img)
+    img = feature.canny(img,sigma = 2)
+    img = util.invert(img)
+    img = Image.fromarray(np.uint8(img)*255)
     img = transform(img)
     input = Variable(img, volatile=True).view(1, -1, 256, 256)
     if opt.cuda:
@@ -46,6 +56,7 @@ for image_name in image_filenames:
     previous = out
     out = out.cpu()
     out_img = out.data[0]
+    #out_img = np.array(out_img).astype(np.uint8)
     if not os.path.exists(os.path.join("result", opt.dataset)):
         os.makedirs(os.path.join("result", opt.dataset))
     save_img(out_img, "result/{}/{}".format(opt.dataset, image_name))
