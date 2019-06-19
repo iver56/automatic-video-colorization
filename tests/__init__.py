4328a922a8aa4d60c1f7df659aa4c72450a16b25
iver56 <iver56@hotmail.com>
2019-06-19 18:09:46 +0200

    Add another test for DatasetFromFolder. Specify valid scipy version range.

---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+import os
+from pathlib import Path
+
+TEST_DATA_DIR = Path(os.path.join(os.path.dirname(__file__), "data"))
+TEST_FRAMES_DIR = TEST_DATA_DIR / "frames"
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,9 +1,20 @@
 import unittest
 
+from torch import Tensor
+
 from tcvc.dataset import DatasetFromFolder
+from tests import TEST_FRAMES_DIR
 
 
 class TestDatasetFromFolder(unittest.TestCase):
+    def test_init_and_len_and_get(self):
+        dataset = DatasetFromFolder(str(TEST_FRAMES_DIR))
+        self.assertEqual(len(dataset), 3)
+        input_image, target, frame_prev = dataset[0]
+        self.assertEqual(type(input_image), Tensor)
+        self.assertEqual(type(target), Tensor)
+        self.assertEqual(type(frame_prev), Tensor)
+
     def test_parse_frame_number(self):
         filename = "22orCRlH-TI_00001.png"
         frame_number, padded_frame_number = DatasetFromFolder.get_frame_number(filename)
