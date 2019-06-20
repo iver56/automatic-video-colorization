92ea5b46b30eb5ce8bf9231414f075207d9423f9
iver56 <iver56@hotmail.com>
2019-06-20 23:15:59 +0200

    Add a script for transferring color from low-res colored frames to high-res greyscale frames

---
+++ tcvc/transfer_colored_frames.py
@@ -0,0 +1,57 @@
+from __future__ import print_function
+
+import argparse
+import os
+
+import numpy as np
+from PIL import Image
+from skimage import img_as_ubyte
+from tqdm import tqdm
+from skimage.color import rgb2hsv, hsv2rgb
+from tcvc.gif import get_file_paths
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Apply the color of the small 256x256 to the corresponding higher-resolution original greyscale frames"
+    )
+    parser.add_argument(
+        "--input-path",
+        dest="input_path",
+        type=str,
+        required=True,
+        help="The path to the folder that contains the greyscale images (frames) and a"
+        ' subfolder named "colored" where the corresponding colored frames reside',
+    )
+    args = parser.parse_args()
+
+    greyscale_file_paths = get_file_paths(args.input_path)
+    colored_file_paths = get_file_paths(os.path.join(args.input_path, "colored"))
+
+    output_path = os.path.join(args.input_path, "colored_full_res")
+    os.makedirs(output_path, exist_ok=True)
+
+    assert len(greyscale_file_paths) == len(colored_file_paths)
+
+    for i, greyscale_file_path in enumerate(tqdm(greyscale_file_paths)):
+        greyscale_image = Image.open(greyscale_file_path)
+        greyscale_image_np = np.array(greyscale_image)
+
+        colored_file_path = colored_file_paths[i]
+        colored_image = Image.open(colored_file_path).resize(
+            greyscale_image.size, resample=Image.LANCZOS
+        )
+        colored_image_np = np.array(colored_image)
+
+        greyscale_image_np_hsv = rgb2hsv(greyscale_image_np)
+        colored_image_np_hsv = rgb2hsv(colored_image_np)
+
+        # Transfer hue and saturation from the color image to the greyscale image
+        greyscale_image_np_hsv[:, :, 0] = colored_image_np_hsv[:, :, 0]  # hue
+        greyscale_image_np_hsv[:, :, 1] = colored_image_np_hsv[:, :, 1]  # saturation
+
+        full_res_colored_image = hsv2rgb(greyscale_image_np_hsv)
+        full_res_colored_image = img_as_ubyte(full_res_colored_image)
+
+        Image.fromarray(full_res_colored_image).save(
+            os.path.join(output_path, greyscale_file_path.name)
+        )
